f08d21a9f242ba98cc999519bf2d9af9ddf71655
Willow-Bot151 <willowhart151@gmail.com>
2024-05-14 11:57:32 +0100

    setup make file tests

--- src/ingestion/utils/config.py
+++ src/ingestion/utils/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-user=os.getenv("PG_USER")
-password=os.getenv("PG_PASSWORD")
-database=os.getenv("PG_DATABASE")
-host=os.getenv("PG_HOST")
-port=int(os.getenv("PG_PORT"))
+user = os.getenv("PG_USER")
+password = os.getenv("PG_PASSWORD")
+database = os.getenv("PG_DATABASE")
+host = os.getenv("PG_HOST")
+port = int(os.getenv("PG_PORT"))
--- src/ingestion/utils/connection.py
+++ src/ingestion/utils/connection.py
@@ -1,11 +1,8 @@
 from pg8000.native import Connection
 from src.ingestion.utils.config import *
 
+
 def connect_to_db():
     return Connection(
-        user=user,
-        password=password,
-        database=database,
-        host=host,
-        port=port
+        user=user, password=password, database=database, host=host, port=port
     )
