8d2872e53a4bd2cf77c524c78a42e97f84d601ef
lUKEdOWNEY <lukedowney2014.ld@gmail.com>
2024-05-17 15:23:03 +0100

    implement a more secure version of connect_to_db which uses aws secertmanager

--- src/ingestion/utils/test_zip/secrets_manager.py
+++ src/ingestion/utils/test_zip/secrets_manager.py
@@ -1,11 +1,13 @@
 import boto3
 from botocore.exceptions import ClientError
-
+#from src.ingestion.utils.test_zip.connection import Connection
+import json
+from pg8000.native import Connection
 
 secrets_client=boto3.client('secretsmanager')
 #secret_name='team_reveries_PSQL'
 
-def get_psql_secret(client):
+def new_connect_to_db(client):
     secret_name = 'team_reveries_PSQL'
     region_name = "eu-west-2"
     # Create a Secrets Manager client
@@ -18,11 +20,19 @@ def get_psql_secret(client):
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
+        secret = get_secret_value_response['SecretString']
+        #print(secret)
+        secret_value = json.loads(secret)
+        username = secret_value['username']
+        password = secret_value['password']
+        database = secret_value['dbname']
+        host = secret_value['host']
+        port = secret_value['port']
+        print(secret_value)
+        return Connection(username, password = password, database = database, host = host, port = port)
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
-    secret = get_secret_value_response['SecretString']
-    print(secret)
 
-    return secret
+   # test = get_psql_secret(secrets_client)
--- testing/ingestion/unit-testing/test_secrets_manager.py
+++ testing/ingestion/unit-testing/test_secrets_manager.py
@@ -1,8 +1,9 @@
-from src.ingestion.utils.test_zip.secrets_manager import get_psql_secret
+from src.ingestion.utils.test_zip.secrets_manager import new_connect_to_db
 import pytest
 import os
 import boto3
 from moto import mock_aws
+from pg8000.native import Connection
 
 @pytest.fixture(scope="class")
 def aws_creds():
@@ -17,15 +18,25 @@ def secrets_client(aws_creds):
     with mock_aws():
         yield boto3.client("secretsmanager")
 
+def test_new_connect_to_db_psql_connection():
+   assert isinstance(new_connect_to_db(secrets_client), Connection)
 
-class TestretrieveSecrets:
-    @staticmethod
-    def test_retrieve_returns_correct_mock_secret_data(secrets_client):
-        secrets_client.create_secret(Name="test", SecretString="""{"UserID": "test", "Password": "testpassword"}""")
-        input='test'
-        result= get_psql_secret(name, client=secrets_client)
-        print(result)
-        assert result == 'test'
+
+
+
+
+
+
+# class TestGetPsqlSecret:
+#     @staticmethod
+#     def test_retrieve_returns_correct_mock_secret_data(secrets_client):
+#         secrets_client.create_secret(Name="test", SecretString="""{"UserID": "test", "Password": "testpassword"}""")
+#         #input='test'
+#         result= get_psql_secret(secrets_client)
+#         expected = {"UserID": "test", "Password": "testpassword"}
+#         expected
+#         print(result)
+#         assert result == expected
 
         # assert os.path.exists('secrets_data.txt')
         # file = open('secrets_data.txt', 'r')
