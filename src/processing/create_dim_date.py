b249b10838e365f9669f0b50c830f762ca47c33b
AnnShelly <annshelly@AnnShellys-MacBook-Pro.local>
2024-05-22 07:13:27 +0100

    adding tests to date

--- src/processing/create_dim_date.py
+++ src/processing/create_dim_date.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def create_dim_date():
+
     date_range = pd.date_range(start="2022-01-01", end="2024-05-21", freq="D")
     dim_date = pd.date_range(start='2022-01-01', end='2024-05-21', freq='D')
     dim_date = pd.DataFrame(date_range, columns=['date_id'])
@@ -11,4 +12,5 @@ def create_dim_date():
     dim_date['day_name'] = dim_date['date_id'].dt.strftime('%A')
     dim_date['month_name'] = dim_date['date_id'].dt.strftime('%B')
     dim_date['quarter'] = dim_date['date_id'].dt.to_period('Q')
+    
     return dim_date
--- testing/processing/unit-testing/test_create_dim_date.py
+++ testing/processing/unit-testing/test_create_dim_date.py
@@ -1,16 +1,17 @@
 from src.processing.create_dim_date import create_dim_date
 import pytest
-import json
-import boto3
-import os
 import pandas as pd
-from moto import mock_aws
 
 class TestDateDimensionTable:
+    def test_create_dim_date_returns_a_dataframe(self):
+        result = create_dim_date()
+        assert isinstance(result, pd.DataFrame)
+
     def test_create_dim_date_has_correct_columns(self):
         expected_columns = ['date_id', 'year', 'month', 'day',
                             'day_of_week', 'day_name', 'month_name', 'quarter']
-        assert list(create_dim_date().columns) == expected_columns
+        result=list(create_dim_date().columns)
+        assert result == expected_columns
 
     def test_create_dim_date_has_correct_data_types(self):
         expected_data_types = {
@@ -26,9 +27,26 @@ class TestDateDimensionTable:
         for col, type in expected_data_types.items():
             assert df[col].dtype == type
 
-    def test_create_dim_date_returns_correct_df_data(self):
+    def test_create_dim_date_returns_correct_data_for_index(self):
        result=create_dim_date()
        assert result['day_of_week'][0] == 5
        assert result['year'][4] == 2022
        assert result['day_name'][868] == 'Saturday'
        assert result['month_name'][867] == 'May'
+
+    def test_create_dim_date_returns_correct_row_data(self):
+        data = {
+            'date_id': [pd.Timestamp('2022-01-01')],
+            'year': [2022],
+            'month': [1],
+            'day': [1],
+            'day_of_week': [5],  # 0=Monday, 6=Sunday, so 5=Saturday
+            'day_name': ['Saturday'],
+            'month_name': ['January'],
+            'quarter': [pd.Period('2022Q1', freq='Q')]
+        }
+        expected_row = pd.DataFrame(data)
+        print(expected_row)
+        result=create_dim_date()
+        print(result.iloc[0])
+        assert result.iloc[0] == expected_row
