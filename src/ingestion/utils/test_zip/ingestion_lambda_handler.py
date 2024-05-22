15135ab27118a2ac7f74ec5d613fbf012a0aab47
Willow-Bot151 <willowhart151@gmail.com>
2024-05-22 18:34:42 +0100

    implemented try except blocks for logging

--- src/ingestion/utils/test_zip/ingestion_lambda_handler.py
+++ src/ingestion/utils/test_zip/ingestion_lambda_handler.py
@@ -22,7 +22,7 @@ def ingestion_lambda_handler(event, context):
         conn = connect_to_db()
     except ClientError as e:
         logger.error("-!ERROR!- No connection to DB returned")
-        raise e("-!ERROR!- No connection to DB returned")
+        raise e("No connection to DB returned")
     table_names = [
         "sales_order",
         "design",
@@ -36,35 +36,48 @@ def ingestion_lambda_handler(event, context):
         "payment",
         "transaction",
     ]
-    s3_client = init_s3_client()
-    logger.error("-!ERROR!- S3 client failed")
+    try:
+        s3_client = init_s3_client()
+    except Exception as e:
+        logger.error("-!ERROR!- S3 client failed")
+        raise e("S3 client failed")
 
-    dt = get_current_timestamp(s3_client)
-    latest_timestamp = get_current_timestamp(s3_client)
-    logger.error(
+
+    try:
+        dt = get_current_timestamp(s3_client)
+        latest_timestamp = get_current_timestamp(s3_client)
+    except Exception as e:
+        logger.error(
         """-!ERROR!- An error occured accessing the timestamp from s3 bucket. 
-                        Please check that there is a timestamp and it's format is correct"""
-    )
+                        Please check that there is a timestamp and it's format is correct""")
+        raise e("An error occured accessing the timestamp from s3 bucket. Please check that there is a timestamp and it's format is correct")
 
     for table in table_names:
-        individual_table = convert_datetimes_and_decimals(
-            query_updated_table_information(conn, table, dt)
-        )
-        logger.error("-!ERROR!- An error occurred in DB query")
+        try:
+            individual_table = convert_datetimes_and_decimals(query_updated_table_information(conn, table, dt))
+        except Exception as e:
+            logger.error("-!ERROR!- An error occurred in DB query")
+            raise e("An error occurred in DB query")
         if len(individual_table[table]) > 0:
-            put_object_in_bucket(
-                table, individual_table, s3_client, "nc-team-reveries-ingestion"
-            )
-            logger.error("-!ERROR!- Failed to put object in bucket")
-
+            try:
+                put_object_in_bucket(table, individual_table, s3_client, "nc-team-reveries-ingestion")
+            except Exception as e:
+                logger.error("-!ERROR!- Failed to put object in bucket")
+                raise e("Failed to put object in bucket")
         if len(individual_table[table]) > 0:
-            potential_timestamp = get_datestamp_from_table(individual_table, table)
+            try:
+                potential_timestamp = get_datestamp_from_table(individual_table, table)
+            except Exception as e:
+                logger.error("-!ERROR!- couldn't retrieve datestamp from table")
+                raise e("couldn't retrieve datestamp from table")
             dt_potential_timestamp = datetime.fromisoformat(potential_timestamp)
             if dt_potential_timestamp > latest_timestamp:
                 latest_timestamp = dt_potential_timestamp
-                logger.error("-!ERROR!- couldn't retrieve datestamp from table")
-
-    put_timestamp_in_s3(latest_timestamp, s3_client)
-    logger.error("failed to put timestamp in bucket")
+                
+    try:
+        put_timestamp_in_s3(latest_timestamp, s3_client)
+    except Exception as e:
+        logger.error("-!ERROR!- failed to put timestamp in bucket")
+        raise e("failed to put timestamp in bucket")
     logger.info("-!STARTPROCESSING!- Ingestion Process is complete.")
     close_connection(conn=conn)
