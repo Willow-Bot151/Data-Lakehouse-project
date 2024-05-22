c2b5f77cfe537002a6679cd3282857db87f07785
lUKEdOWNEY <lukedowney2014.ld@gmail.com>
2024-05-22 17:08:18 +0100

    add error handling and logging to our lambda handler

--- src/ingestion/utils/test_zip/ingestion_lambda_handler.py
+++ src/ingestion/utils/test_zip/ingestion_lambda_handler.py
@@ -9,12 +9,14 @@ from utils import (
     put_timestamp_in_s3,
     convert_datetimes_and_decimals,
 )
-
-
+import logging
+from botocore.exceptions import ClientError
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 def ingestion_lambda_handler(event, context):
-
+    logger.info("Ingestion process beginning")
     conn = connect_to_db()
-
+    logger.error('-!ERROR!- No connection to DB returned')
     table_names = [
         "sales_order",
         "design",
@@ -28,28 +30,45 @@ def ingestion_lambda_handler(event, context):
         "payment",
         "transaction",
     ]
-    s3_client = init_s3_client()
-    dt = get_current_timestamp(s3_client)
-    latest_timestamp = get_current_timestamp(s3_client)
+    try:
+        s3_client = init_s3_client()
+    except ClientError as e:
+        logger.error("-!ERROR!- S3 client failed")
+        close_connection(conn)
+        raise e
+    
+    try:
+        dt = get_current_timestamp(s3_client)
+        latest_timestamp = get_current_timestamp(s3_client)
+    except Exception as e:
+        logger.error("""-!ERROR!- An error occured accessing the timestamp from s3 bucket. 
+                        Please check that there is a timestamp and it's format is correct""")
+        close_connection(conn)
+        raise e("failed to get timestamp")
 
     for table in table_names:
         individual_table = convert_datetimes_and_decimals(
             query_updated_table_information(conn, table, dt)
         )
-
+        logger.error("-!ERROR!- An error occurred in DB query")
         if len(individual_table[table]) > 0:
-            # print("I have arrived!")
             put_object_in_bucket(
                 table, individual_table, s3_client, "nc-team-reveries-ingestion"
             )
-
+            logger.error("-!ERROR!- Failed to put object in bucket")
+        
+        
         if len(individual_table[table]) > 0:
-            potential_timestamp = get_datestamp_from_table(individual_table, table)
-            dt_potential_timestamp = datetime.fromisoformat(potential_timestamp)
-            if dt_potential_timestamp > latest_timestamp:
-                latest_timestamp = dt_potential_timestamp
+            try:
+                potential_timestamp = get_datestamp_from_table(individual_table, table)
+                dt_potential_timestamp = datetime.fromisoformat(potential_timestamp) 
+                if dt_potential_timestamp > latest_timestamp:
+                    latest_timestamp = dt_potential_timestamp
+            except Exception as e:
+                logger.error("-!ERROR!- couldn't retrieve datestamp from table")
+                raise e
 
     put_timestamp_in_s3(latest_timestamp, s3_client)
-
+    logger.error("failed to put timestamp in bucket")
+    logger.info("-!STARTPROCESSING!- Ingestion Process is complete.")
     close_connection(conn=conn)
-
