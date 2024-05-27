3f3afc86444ba05bed3ae5bbdcf4fca3758e914d
Willow-Bot151 <willowhart151@gmail.com>
2024-05-27 15:01:30 +0100

    made dim_design function

--- src/processing/create_dim_design.py
+++ src/processing/create_dim_design.py
@@ -1,29 +1,12 @@
 import pandas as pd
 
-def create_dim_counterparty(address_df, counterparty_df):
-    renamed_counterparty_df = counterparty_df.rename(columns={
-        "legal_address_id":"address_id"
-        })
-    renamed_address_df = address_df.rename(columns={
-        "address_line_1":"counterparty_legal_address_line_1",
-        "address_line_2":"counterparty_legal_address_line2",
-        "district":"counterparty_legal_district",
-        "city":"counterparty_legal_city",
-        "postal_code":"counterparty_legal_postal_code",
-        "country":"counterparty_legal_country",
-        "phone":"counterparty_legal_phone_number"
-        })
-    big_df = pd.merge(renamed_address_df, renamed_counterparty_df, on="address_id")
+def create_dim_design(design_df):
+    print(design_df.columns)
     required_columns = [
-        'counterparty_id',
-        'counterparty_legal_name',
-        'counterparty_legal_address_line_1',
-        'counterparty_legal_address_line2',
-        'counterparty_legal_district',
-        'counterparty_legal_city',
-        'counterparty_legal_postal_code',
-        'counterparty_legal_country',
-        'counterparty_legal_phone_number'
+            "design_id",
+            "design_name",
+            "file_location",
+            "file_name"
         ]
-    dim_df = big_df.filter(required_columns)
-    return dim_df
+    dim_design_df = design_df.filter(required_columns)
+    return dim_design_df
