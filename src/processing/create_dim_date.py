c372a0b49dcdd6746f7e7fe7040dcecb612960ac
Willow-Bot151 <willowhart151@gmail.com>
2024-05-29 15:18:47 +0100

    fix datetime?

--- src/processing/create_dim_date.py
+++ src/processing/create_dim_date.py
@@ -2,12 +2,9 @@ import pandas as pd
 from datetime import datetime, date, time
 
 def create_dim_date():
-
-    date_range = pd.date_range(start="2022-01-01", end="2025-05-21", freq="D")
+    date_range = pd.date_range(start="2022-01-01", end="2024-05-21", freq="D")
     dim_date = pd.DataFrame(date_range, columns=['date_id'])
-    dim_date['date_id'] = dim_date['date_id'].apply(
-        lambda x: datetime.fromisoformat(x).date()
-        )
+    dim_date['date_id'] = pd.to_datetime(dim_date['date_id'])
     dim_date['year'] = dim_date['date_id'].dt.year
     dim_date['month'] = dim_date['date_id'].dt.month
     dim_date['day'] = dim_date['date_id'].dt.day
@@ -15,8 +12,7 @@ def create_dim_date():
     dim_date['day_name'] = dim_date['date_id'].dt.strftime('%A')
     dim_date['month_name'] = dim_date['date_id'].dt.strftime('%B')
     dim_date['quarter'] = dim_date['date_id'].dt.quarter
-    
     return dim_date
 
 def update_dim_date():
-    pass
+    pass 
